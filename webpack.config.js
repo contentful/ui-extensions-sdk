0d203361853851e60a3b66f20916a8626c5ae766
Thomas Scholtes <thomas-scholtes@gmx.de>
2015-11-03 12:51:43 +0100

    include api prototype

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path')
+
+module.exports = {
+  entry: {
+    'cf-widget-api': './lib/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+}
