65fce347dcf2b24890ce0e716aa3804160d9b2ae
Jakub Elżbieciak <jakub@contentful.com>
2019-01-22 09:55:09 +0100

    Add navigator methods

---
+++ test/unit/navigator.js
@@ -0,0 +1,60 @@
+const { describeChannelCallingMethod } = require('../helpers')
+
+const createNavigator = require('../../lib/navigator')
+
+const SCENARIOS = [
+  {
+    method: 'openEntry',
+    args: ['entry-id'],
+    expected: { entityType: 'Entry', id: 'entry-id' }
+  },
+  {
+    method: 'openEntry',
+    args: ['entry-id', { slideIn: true }],
+    expected: { entityType: 'Entry', id: 'entry-id', slideIn: true }
+  },
+  {
+    method: 'openNewEntry',
+    args: ['ct-id'],
+    expected: { entityType: 'Entry', id: null, contentTypeId: 'ct-id' }
+  },
+  {
+    method: 'openNewEntry',
+    args: ['ct-id', { slideIn: true }],
+    expected: { entityType: 'Entry', id: null, contentTypeId: 'ct-id', slideIn: true }
+  },
+  {
+    method: 'openAsset',
+    args: ['asset-id'],
+    expected: { entityType: 'Asset', id: 'asset-id' }
+  },
+  {
+    method: 'openAsset',
+    args: ['asset-id', { slideIn: true }],
+    expected: { entityType: 'Asset', id: 'asset-id', slideIn: true }
+  },
+  {
+    method: 'openNewAsset',
+    args: [],
+    expected: { entityType: 'Asset', id: null }
+  },
+  {
+    method: 'openNewAsset',
+    args: [{ slideIn: true }],
+    expected: { entityType: 'Asset', id: null, slideIn: true }
+  }
+]
+
+describe('createNavigator()', () => {
+  describe('returned "navigator" object', () => {
+    SCENARIOS.forEach(({ method, args, expected }) => {
+      describeChannelCallingMethod({
+        creator: createNavigator,
+        methodName: method,
+        channelMethod: 'navigate',
+        args,
+        expectedCallArgs: expected
+      })
+    })
+  })
+})
