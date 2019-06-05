921389a5e69fc1b8f45e8a378e42f822376c508e
Alex Suevalov <alex.suevalov@contentful.com>
2019-06-05 17:57:40 +0200

    Fetch and wait environment to be created

--- test/integration/tasks/create-new-enviromenment.js
+++ test/integration/tasks/create-new-enviromenment.js
@@ -2,6 +2,14 @@ const nanoid = require('nanoid')
 const getCurrentSpace = require('../contentful-client').getCurrentSpace
 const printStepTitle = require('../utils').printStepTitle
 
+const delay = num => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, num)
+  })
+}
+
 module.exports = async () => {
   printStepTitle('Creating a new environment for testing')
 
@@ -9,7 +17,19 @@ module.exports = async () => {
 
   const environmentId = nanoid()
 
-  await space.createEnvironmentWithId(environmentId, { name: environmentId }, 'master')
+  let environment = await space.createEnvironmentWithId(
+    environmentId,
+    { name: environmentId },
+    'master'
+  )
+
+  let status = environment.sys.status.sys.id
+
+  while (status !== 'ready') {
+    await delay(1500)
+    environment = await space.getEnvironment(environmentId)
+    status = environment.sys.status.sys.id
+  }
 
   console.log(`New "${environmentId}" environment is created`)
 
