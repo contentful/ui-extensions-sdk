88faf4e2598235eb25414a1d7fb5702364e31783
David Fateh <davidfateh@gmail.com>
2019-06-20 10:37:51 +0200

    using function for running extension scripts

--- test/integration/tasks/deploy-extensions.js
+++ test/integration/tasks/deploy-extensions.js
@@ -3,6 +3,10 @@ const printStepTitle = require('../utils').printStepTitle
 
 module.exports = async () => {
   printStepTitle('Deploy extensions to a new environment')
-  await runScript('npm', ['run', 'deploy', '--prefix', 'test/extensions/test-field-extension'])
-  await runScript('npm', ['run', 'deploy', '--prefix', 'test/extensions/test-page-extension'])
+
+  async function runExtensionScript(extensionId) {
+    await runScript('npm', ['run', 'deploy', '--prefix', `test/extensions/${extensionId}`])
+  }
+
+  ;['test-field-extension', 'test-page-extension'].forEach(runExtensionScript)
 }
