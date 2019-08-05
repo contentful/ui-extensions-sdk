899a2ae81e6c2891445c32d349ac40ec3f7fa2bd
Maryna Glukhodid <maryna@contentful.com>
2019-08-05 15:32:23 +0200

    Changed JSON.stringify to deep.equal

--- test/cypress/integration/reusable/open-sdk-locales-data-test.js
+++ test/cypress/integration/reusable/open-sdk-locales-data-test.js
@@ -2,7 +2,7 @@ const localesData = require('../fixtures/locales-data.json')
 
 export function verifySdkLocalesData(iframeSelector) {
   cy.getSdk(iframeSelector).then(sdk => {
-    expect(JSON.stringify(sdk.locales)).to.equal(JSON.stringify(localesData))
+    expect(sdk.locales).to.deep.equal(localesData)
   })
 }
 
--- test/cypress/integration/reusable/open-sdk-user-data-test.js
+++ test/cypress/integration/reusable/open-sdk-user-data-test.js
@@ -2,12 +2,12 @@ const userData = require('../fixtures/user-data.json')
 
 export function verifySdkUserData(iframeSelector) {
   cy.getSdk(iframeSelector).then(sdk => {
-    expect(JSON.stringify(sdk.user)).to.equal(JSON.stringify(userData))
+    expect(sdk.user).to.deep.equal(userData)
   })
 }
 
 export function openSdkUserDataTest(iframeSelector) {
-  it('sdk.user static methods have expected values', () => {
+  it.only('sdk.user static methods have expected values', () => {
     verifySdkUserData(iframeSelector)
   })
 }
