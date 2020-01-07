a2028c3ae0109495f2ab32d037064cf433453be5
Alex Suevalov <alex.suevalov@contentful.com>
2020-01-07 09:42:38 +0100

    updated test data

--- test/cypress/integration/reusable/open-sdk-locales-data-test.js
+++ test/cypress/integration/reusable/open-sdk-locales-data-test.js
@@ -1,8 +1,16 @@
-const localesData = require('../fixtures/locales-data.json')
+const expectedLocaleData = {
+  available: ['en-US', 'ru'],
+  default: 'en-US',
+  names: { 'en-US': 'English (United States)', ru: 'Russian' },
+  fallbacks: {
+    'en-US': undefined,
+    ru: 'en-US'
+  }
+}
 
 export function verifySdkLocalesData(iframeSelector) {
   cy.getSdk(iframeSelector).then(sdk => {
-    expect(sdk.locales).to.deep.equal(localesData)
+    expect(sdk.locales).to.deep.equal(expectedLocaleData)
   })
 }
 
