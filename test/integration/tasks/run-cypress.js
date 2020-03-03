d91002495761a5b8e66978dd87e458e991aeafcb
Alex Suevalov <alex.suevalov@contentful.com>
2020-03-03 14:28:52 +0100

    Improve integration reports

--- test/integration/tasks/run-cypress.js
+++ test/integration/tasks/run-cypress.js
@@ -3,11 +3,5 @@ const printStepTitle = require('../utils').printStepTitle
 
 module.exports = async () => {
   printStepTitle('Runnings tests...')
-  await runScript('./node_modules/.bin/cypress', [
-    'run',
-    '--browser',
-    'chrome',
-    '--reporter',
-    'mochawesome'
-  ])
+  await runScript('./node_modules/.bin/cypress', ['run', '--browser', 'chrome'])
 }
