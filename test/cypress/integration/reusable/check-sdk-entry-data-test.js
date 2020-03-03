be8c82827cfcc06cbaa60da587664eadb662502c
Alex Suevalov <alex.suevalov@contentful.com>
2020-03-03 14:42:37 +0100

    Fix check-sdk-entry-data test

--- test/cypress/integration/reusable/check-sdk-entry-data-test.js
+++ test/cypress/integration/reusable/check-sdk-entry-data-test.js
@@ -2,7 +2,12 @@ const entryData = require('../fixtures/entry-data.json')
 
 export function verifySdkEntryData(iframeSelector) {
   cy.getSdk(iframeSelector).then(sdk => {
-    expect(sdk.entry.fields).to.deep.equal(entryData.fields)
+    const keys = Object.keys(sdk.entry.fields)
+    keys.forEach(key => {
+      ;['id', 'locales', 'type', 'required', 'validations'].forEach(property => {
+        expect(sdk.entry.fields[key][property]).deep.equal(entryData.fields[key][property])
+      })
+    })
   })
 }
 
