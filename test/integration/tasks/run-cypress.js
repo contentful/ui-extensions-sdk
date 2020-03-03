ff5af7cc3e23946f09889c064ed9f6cc4ef45e30
Alex Suevalov <alex.suevalov@contentful.com>
2020-03-03 15:03:12 +0100

    Run tests in parallel

--- test/integration/tasks/run-cypress.js
+++ test/integration/tasks/run-cypress.js
@@ -3,5 +3,11 @@ const printStepTitle = require('../utils').printStepTitle
 
 module.exports = async () => {
   printStepTitle('Runnings tests...')
-  await runScript('./node_modules/.bin/cypress', ['run', '--browser', 'chrome'])
+  await runScript('./node_modules/.bin/cypress', [
+    'run',
+    '--record',
+    '--browser',
+    'chrome',
+    '--parallel'
+  ])
 }
