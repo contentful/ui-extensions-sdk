58fbbe83561f5a902427e04c9520efac6acc5bb9
Alex Suevalov <alex.suevalov@contentful.com>
2020-03-06 13:59:37 +0100

    Added integration test

---
+++ test/cypress/integration/reusable/check-sdk-navigation-slide-in-callback-test.js
@@ -0,0 +1,15 @@
+import * as Constants from '../../../constants'
+
+export function checkSdkNavigationSlideInCallbackTest(iframeSelector) {
+  it('sdk.navigator.onSlideInNavigation is called when slide-in editor is opened and when it is closed', () => {
+    cy.getSdk(iframeSelector).then(async sdk => {
+      const spy = cy.stub()
+      sdk.navigator.onSlideInNavigation(spy)
+      await sdk.navigator.openEntry(Constants.entries.testImageWrapper, {
+        slideIn: true
+      })
+      expect(spy).to.be.calledOnce
+      expect(spy).to.be.calledWith({ newSlideLevel: 1, oldSlideLevel: 0 })
+    })
+  })
+}
