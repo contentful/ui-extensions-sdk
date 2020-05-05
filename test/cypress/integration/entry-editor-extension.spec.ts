2ad7469820600f565996a03016ec043edbb7b8a4
Felix Boenke <felix.boenke@Felixs-MBP.fritz.box>
2020-05-05 14:50:05 +0200

    move test order

--- test/cypress/integration/entry-editor-extension.spec.ts
+++ test/cypress/integration/entry-editor-extension.spec.ts
@@ -94,6 +94,9 @@ context('Entry editor extension', () => {
 
   /* Reusable tests */
 
+  checkSdkNavigationSlideInCallbackTest(iframeSelector)
+  openSuccessNotificationTest(iframeSelector)
+  openErrorNotificationTest(iframeSelector)
   openPageExtensionTest(iframeSelector)
   openDialogExtensionTest(iframeSelector)
   openEntryTest(iframeSelector)
@@ -104,7 +107,4 @@ context('Entry editor extension', () => {
   openSdkLocalesDataTest(iframeSelector)
   checkSdkEntryDataTest(iframeSelector)
   checkSdkSpaceMethods(iframeSelector)
-  openSuccessNotificationTest(iframeSelector)
-  openErrorNotificationTest(iframeSelector)
-  checkSdkNavigationSlideInCallbackTest(iframeSelector)
 })
