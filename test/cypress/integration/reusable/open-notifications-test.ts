2daaa8a3e1d4e60e28462b1fc7efd10cbbc2b075
Felix Boenke <felix.boenke@Felixs-MBP.fritz.box>
2020-05-06 15:53:57 +0200

    chain notifcation assertions

--- test/cypress/integration/reusable/open-notifications-test.ts
+++ test/cypress/integration/reusable/open-notifications-test.ts
@@ -1,21 +1,27 @@
 export function verifySuccessNotification(iframeSelector) {
   const successMessage = 'Success message!'
-  cy.getSdk(iframeSelector).then(sdk => {
-    sdk.notifier.success(successMessage)
-  })
-  cy.findByTestId('cf-ui-notification')
-    .should('have.attr', 'data-intent', 'success')
-    .and('contain', successMessage)
+  cy.getSdk(iframeSelector)
+    .then(sdk => {
+      sdk.notifier.success(successMessage)
+    })
+    .then(() => {
+      cy.findByTestId('cf-ui-notification')
+        .should('have.attr', 'data-intent', 'success')
+        .and('contain', successMessage)
+    })
 }
 
 export function verifyErrorNotification(iframeSelector) {
   const errorMessage = 'Error message!'
-  cy.getSdk(iframeSelector).then(sdk => {
-    sdk.notifier.error(errorMessage)
-  })
-  cy.findByTestId('cf-ui-notification')
-    .should('have.attr', 'data-intent', 'error')
-    .and('contain', errorMessage)
+  cy.getSdk(iframeSelector)
+    .then(sdk => {
+      sdk.notifier.error(errorMessage)
+    })
+    .then(() => {
+      cy.findByTestId('cf-ui-notification')
+        .should('have.attr', 'data-intent', 'error')
+        .and('contain', errorMessage)
+    })
 }
 
 export function openSuccessNotificationTest(iframeSelector) {
