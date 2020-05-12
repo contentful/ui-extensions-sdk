c308df431d4dcf41d275224bfd880f25b13307d7
Felix Boenke <felix.boenke@Felixs-MBP.fritz.box>
2020-05-12 16:10:21 +0200

    run script seperated and without cleanup right now

--- test/integration/cleanup.ts
+++ test/integration/cleanup.ts
@@ -1,23 +1,4 @@
-import deleteEnvironment from './tasks/delete-new-environment'
-import asyncRetry from 'async-retry'
-
-const environmentId = Cypress.env('activeEnvironmentId')
-
-const cleanup = async () => {
-  if (environmentId) {
-    try {
-      await asyncRetry(
-        () => {
-          return deleteEnvironment(environmentId)
-        },
-        { retries: 3 }
-      )
-    } catch (e) {
-      console.log(e)
-      throw new Error('Failed to remove environment')
-    }
-  }
-}
+import { cleanup } from './setup'
 ;(async function main() {
   try {
     await cleanup()
