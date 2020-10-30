c46be9db35f0a6b72cb50851009b836146235b31
John Whiles <john.whiles@contentful.com>
2020-10-30 12:51:44 +0100

    feat: log error if it appears app is running outside of iframe (#390)

--- module-declaration.d.ts
+++ module-declaration.d.ts
@@ -1,6 +1,9 @@
 // will be appended to generated typings.d.ts
 declare module 'contentful-ui-extensions-sdk' {
-  export const init: <T extends KnownSDK = KnownSDK>(initCallback: (sdk: T) => any) => void
+  export const init: <T extends KnownSDK = KnownSDK>(
+    initCallback: (sdk: T) => any,
+    options?: { supressIframeWarning?: boolean }
+  ) => void
 
   export const locations: Locations
 }
