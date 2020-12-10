e3476b880d2fa1fd04a1d419e01e66ca9f8eeffb
Andi Pätzold <andreas.paetzold@contentful.com>
2020-12-10 15:28:31 +0100

    chore: run cypress tests in parallel

--- test/integration/tasks/run-cypress.ts
+++ test/integration/tasks/run-cypress.ts
@@ -1,7 +1,73 @@
-import runScript from '../run-script'
+import spawn from 'cross-spawn'
+import fs from 'fs'
+import glob from 'glob'
+import path from 'path'
+import os from 'os'
 import { printStepTitle } from '../utils'
 
 export default async () => {
   printStepTitle('Runnings tests...')
-  await runScript('./node_modules/.bin/cypress', ['run', '--browser', 'chrome'])
+
+  const cypressJSONRaw = fs.readFileSync(path.resolve(__dirname, '../../../cypress.json'), {
+    encoding: 'UTF-8',
+  })
+  const cypressJSON = JSON.parse(cypressJSONRaw)
+
+  const testFiles = await new Promise<string[]>((resolve, reject) =>
+    glob(
+      path.resolve(
+        cypressJSON.integrationFolder || 'cypress/integration',
+        cypressJSON.testFiles || '**/*.*'
+      ),
+      {
+        cwd: path.resolve(__dirname, '../../..'),
+      },
+      (error, matches) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(matches)
+      }
+    )
+  )
+
+  const chunk1 = testFiles.slice(0, testFiles.length / 2)
+  const chunk2 = testFiles.slice(testFiles.length / 2, testFiles.length)
+
+  await Promise.all([chunk1, chunk2].map((chunk, chunkIndex) => run(chunk, chunkIndex)))
+}
+
+async function run(specFiles: string[], index: number) {
+  return await runScript(
+    './node_modules/.bin/cypress',
+    ['run', '--browser', 'chrome', '--spec', specFiles.join(',')],
+    index
+  )
+}
+
+async function runScript(command: string, args: any, index: number) {
+  const XDG_CONFIG_HOME = path.resolve(process.env.TMPDIR || os.tmpdir(), `cypress-${index}`)
+  fs.mkdirSync(XDG_CONFIG_HOME, { recursive: true })
+
+  await new Promise<void>((resolve, reject) => {
+    let data = ''
+    const child = spawn(command, args, {
+      stdio: 'pipe',
+      env: {
+        ...process.env,
+        XDG_CONFIG_HOME,
+      },
+    })
+    child.stdout!.on('data', (message) => {
+      data += message
+    })
+    child.on('close', (code: number) => {
+      console.log(data)
+      if (code !== 0) {
+        reject(new Error(`${command} ${args.join(' ')}. Exit code: ${code}`))
+      } else {
+        resolve()
+      }
+    })
+  })
 }
