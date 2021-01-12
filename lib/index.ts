47b75a2a24d8e6ca52b818add11e24be0d5a3db6
Andi Pätzold <andreas.paetzold@contentful.com>
2021-01-12 15:46:43 +0100

    fix: do not change type of init

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,13 @@
 import createInitializer from './initialize'
 import createAPI from './api'
+import { KnownSDK } from './types'
 
 export * from './types'
 export { default as locations } from './locations'
-export const init = createInitializer(window, createAPI)
+
+type Init = <T extends KnownSDK = KnownSDK>(
+  initCallback: (sdk: T) => any,
+  options?: { supressIframeWarning?: boolean }
+) => void
+
+export const init = createInitializer(window, createAPI) as Init
