7d9fc1bc8f4cc85731c700f8dfcc00e7bf8802ce
Manuel Spagnolo <spagnolo.manu@gmail.com>
2021-06-04 11:29:32 +0200

    chore: add tracking for old uie assets

--- docs/tagged/cf-extension-api-v2.js
+++ docs/tagged/cf-extension-api-v2.js
@@ -84,7 +84,22 @@ return /******/ (function(modules) { // webpackBootstrap
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	var init = exports.init = (0, _initialize2.default)(createWidgetAPI);
-	
+
+    (function () {
+      try {
+        var ct = contentType
+        var sid
+        if (ct.sys && ct.sys.space && ct.sys.space.sys && ct.sys.space.sys.id) {
+          sid = ct.sys.space.sys.id
+        }
+        if (sid) {
+          fetch('https://api.contentful.com/uie-tracking?sid=' + sid)
+        }
+      } catch (e) {
+        // noop
+      }
+    })();
+
 	function createWidgetAPI(channel, _ref) {
 	  var entry = _ref.entry;
 	  var locales = _ref.locales;
