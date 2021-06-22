20cc07943bd32081a9148d5941a7c656712493e5
Manuel Spagnolo <spagnolo.manu@gmail.com>
2021-06-22 11:31:31 +0200

    chore: circle globs

--- test/integration/tasks/run-cypress.ts
+++ test/integration/tasks/run-cypress.ts
@@ -3,7 +3,7 @@ import { printStepTitle } from '../utils'
 
 export default async (role: string, initializeTestOnly: boolean = false) => {
   printStepTitle(`${role}: Runnings tests...`)
-  const args = ['run', '--browser', 'chrome', '--parallel', '--record', '--key=uie-integration']
+  const args = ['run', '--browser', 'chrome']
   if (initializeTestOnly) {
     args.push('--spec', 'test/cypress/integration/initialize.spec.ts')
   }
