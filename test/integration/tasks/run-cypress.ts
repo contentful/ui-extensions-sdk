3ca0cecca874ce387b9cedf063546a206ed9a053
Manuel Spagnolo <spagnolo.manu@gmail.com>
2021-06-22 12:45:27 +0200

    chore: some more logs

--- test/integration/tasks/run-cypress.ts
+++ test/integration/tasks/run-cypress.ts
@@ -7,7 +7,13 @@ export default async (role: string, initializeTestOnly: boolean = false) => {
   if (initializeTestOnly) {
     args.push('--spec', 'test/cypress/integration/initialize.spec.ts')
   } else if (process.env.TEST) {
+    console.log('Testing the following test files: ', process.env.TEST.split(/\s/).join(','))
+    // allow circle to pick a subset of test files
     args.push('--spec', process.env.TEST.split(/\s/).join(','))
+  } else {
+    // otherwise default
+    console.log('Using default glob')
+    args.push('--spec', '**/*.spec.[jt]s')
   }
   await runScript('./node_modules/.bin/cypress', args)
 }
