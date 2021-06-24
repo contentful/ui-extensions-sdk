0b94bce8b8d8ec9e5a482d6bd862c1eea34f9995
Manuel Spagnolo <spagnolo.manu@gmail.com>
2021-06-24 15:42:09 +0200

    chore: logging

--- test/integration/contentful-client.ts
+++ test/integration/contentful-client.ts
@@ -4,6 +4,8 @@ if (!process.env.CONTENTFUL_CMA_TOKEN) {
   require('dotenv').config()
 }
 
+console.log('CONTENTFUL_HOST', (process.env.CONTENTFUL_HOST || '').split('').reverse())
+
 export const client = createClient({
   host: process.env.CONTENTFUL_HOST || 'api.contentful.com',
   accessToken: process.env.CONTENTFUL_CMA_TOKEN as string,
