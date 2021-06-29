b7e4eac6775d35a08a3648cdbfac8c348ef0b503
Manuel Spagnolo <spagnolo.manu@gmail.com>
2021-06-29 14:20:22 +0200

    fix: create entry with environmentId

--- test/integration/tasks/copy-entries.ts
+++ test/integration/tasks/copy-entries.ts
@@ -4,11 +4,13 @@ import { printStepTitle } from '../utils'
 export default async function copyEntries(entryIds: Record<string, string>) {
   printStepTitle('Copying entries from test-base to master-test')
 
+  const environmentId = 'test-base'
+
   const newEntryIds: Record<string, string> = {}
   for (const [entryLabel, entryId] of Object.entries(entryIds)) {
-    const entry = await plainClient.entry.get({ entryId, environmentId: 'test-base' })
+    const entry = await plainClient.entry.get({ entryId, environmentId })
     const newEntry = await plainClient.entry.createWithId(
-      { entryId, contentTypeId: entry.sys.contentType.sys.id },
+      { entryId, contentTypeId: entry.sys.contentType.sys.id, environmentId },
       entry
     )
     newEntryIds[entryLabel] = newEntry.sys.id
