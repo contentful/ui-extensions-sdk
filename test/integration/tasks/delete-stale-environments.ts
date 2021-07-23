9c1c84dd550aa276d7c40b6fa09b2ea47f14b4da
Lorenz Weiß <34910067+loweisz@users.noreply.github.com>
2021-07-23 16:25:01 +0200

    fix: Update canary with master (#879)

--- test/integration/tasks/delete-stale-environments.ts
+++ test/integration/tasks/delete-stale-environments.ts
@@ -1,38 +1,43 @@
+import { EnvironmentProps } from 'contentful-management/types'
 import { plainClient } from '../contentful-client'
-import { printStepTitle, sleep } from '../utils'
+import { printStepTitle } from '../utils'
 
 const TWO_HOURS_IN_MS = 60 * 60 * 2 * 1000
 
 export default async (client = plainClient) => {
   printStepTitle('Removing stale environments')
 
-  const environments = await client.environment.getMany({})
-  const { items } = environments
+  const environmentCollection = await client.environment.getMany({})
+  const { items: environments } = environmentCollection
 
   // filter for relevant environments
-  const isProtected = (name: string) => name === 'master' || name.includes('test')
+  const isProtected = (environment: EnvironmentProps) =>
+    environment.name === 'master' || environment.name.includes('test')
 
-  const isStaleEnvironment = (timeStamp: string) => {
-    const environmentDate = new Date(timeStamp).getTime()
+  const isStaleEnvironment = (environment: EnvironmentProps) => {
+    const environmentDate = new Date(environment.sys.createdAt).getTime()
     const difference = Date.now() - environmentDate
     return difference >= TWO_HOURS_IN_MS
   }
-  const deletedEnvironmentIds: string[] = []
-  items.forEach(async (environment: any) => {
-    const {
-      name,
-      sys: { createdAt, id },
-    } = environment
-    if (!isProtected(name) && isStaleEnvironment(createdAt)) {
-      try {
-        await environment.delete()
-        deletedEnvironmentIds.push(id)
-        await sleep(200)
-      } catch (error) {
-        console.error(`Could not delete environment ${environment.sys.id}`)
-      }
-    }
-  })
 
-  return deletedEnvironmentIds
+  const promiseResults = await Promise.allSettled(
+    environments
+      .filter((environment) => !isProtected(environment))
+      .filter((environment) => isStaleEnvironment(environment))
+      .map(async (environment) => {
+        const id = environment.sys.id
+
+        try {
+          await client.environment.delete({ environmentId: id })
+          console.log(`Deleted environment ${id}`)
+          return id
+        } catch (error) {
+          console.error(`Could not delete environment ${id}`)
+        }
+      })
+  )
+
+  return promiseResults
+    .filter((r): r is PromiseFulfilledResult<string> => r.status === 'fulfilled')
+    .map((r) => r.value)
 }
