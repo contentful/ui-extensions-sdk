fd4fce69cc66314f4e75870538c1d9c9604712b7
Manuel Spagnolo <spagnolo.manu@gmail.com>
2021-09-08 15:41:26 +0200

    chore: log the full error on uncaught exceptions

--- test/cypress/support/index.ts
+++ test/cypress/support/index.ts
@@ -5,3 +5,10 @@ import { configure } from '@testing-library/dom'
 import './commands'
 
 configure({ testIdAttribute: 'data-test-id' })
+
+Cypress.on('uncaught:exception', (error: Error) => {
+  console.log('An unexpected exception has been thrown in your apps code.')
+  console.error(error)
+
+  return error
+})
