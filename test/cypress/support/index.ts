63e66483198534b46e281712a69d31bb5abdcf88
Manuel Spagnolo <spagnolo.manu@gmail.com>
2021-09-09 16:18:36 +0200

    chore: do not fail on unhandled

--- test/cypress/support/index.ts
+++ test/cypress/support/index.ts
@@ -7,9 +7,5 @@ require('cypress-terminal-report/src/installLogsCollector')()
 
 configure({ testIdAttribute: 'data-test-id' })
 
-Cypress.on('uncaught:exception', (error, runnable) => {
-  console.log('Found an uncaught error')
-  console.log(error)
-  console.log(runnable)
-  //return false
-})
+// Don't fail suite in case of uncaught exceptions, but delegate to assertions
+Cypress.on('uncaught:exception', (error, runnable) => false)
