662969d0798d5576ab708e9a705a8a9221e91649
Manuel Spagnolo <spagnolo.manu@gmail.com>
2021-09-09 16:00:05 +0200

    chore: add more logs to the suite

--- test/cypress/support/index.ts
+++ test/cypress/support/index.ts
@@ -3,5 +3,13 @@
 import { configure } from '@testing-library/dom'
 
 import './commands'
+require('cypress-terminal-report/src/installLogsCollector')()
 
 configure({ testIdAttribute: 'data-test-id' })
+
+Cypress.on('uncaught:exception', (error, runnable) => {
+  console.log('Found an uncaught error')
+  console.log(error)
+  console.log(runnable)
+  //return false
+})
