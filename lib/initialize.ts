9fd72fdbe282564ed0e09376589690dde92a287c
Andi Pätzold <andreas.paetzold@contentful.com>
2022-02-09 10:54:06 +0100

    fix: return same SDK instance when calling `init` multiple times

--- lib/initialize.ts
+++ lib/initialize.ts
@@ -5,9 +5,10 @@ export default function createInitializer(
   currentWindow: Window,
   apiCreator: (channel: Channel, data: ConnectMessage, window: Window) => KnownSDK
 ) {
-  const connectDeferred = createDeferred()
+  const connectDeferred =
+    createDeferred<[channel: Channel, message: ConnectMessage, messageQueue: unknown[]]>()
 
-  connectDeferred.promise.then(([channel]: [Channel]) => {
+  connectDeferred.promise.then(([channel]) => {
     const { document } = currentWindow
     document.addEventListener('focus', () => channel.send('setActive', true), true)
     document.addEventListener('blur', () => channel.send('setActive', false), true)
@@ -17,6 +18,7 @@ export default function createInitializer(
   // messages before `init` is called.
   connect(currentWindow, (...args) => connectDeferred.resolve(args))
 
+  let initializedSdks: Promise<[sdk: KnownSDK, customSdk: any]> | undefined
   return function init(
     initCb: (sdk: KnownSDK, customSdk: any) => any,
     {
@@ -34,26 +36,33 @@ In order for the ui-extension-sdk to function correctly, your app needs to be ru
 Learn more about local development with the ui-extension-sdk here:
   https://www.contentful.com/developers/docs/extensibility/ui-extensions/faq/#how-can-i-use-the-ui-extension-sdk-locally`)
     }
-    connectDeferred.promise.then(
-      ([channel, params, messageQueue]: [Channel, ConnectMessage, unknown[]]) => {
-        const api = apiCreator(channel, params, currentWindow)
 
-        let customApi
-        if (typeof makeCustomApi === 'function') {
-          customApi = makeCustomApi(channel, params)
-        }
+    if (!initializedSdks) {
+      initializedSdks = new Promise((resolve) => {
+        connectDeferred.promise.then(([channel, params, messageQueue]) => {
+          const api = apiCreator(channel, params, currentWindow)
 
-        // Handle pending incoming messages.
-        // APIs are created before so handlers are already
-        // registered on the channel.
-        messageQueue.forEach((m) => {
-          // TODO Expose private handleMessage method
-          ;(channel as any)._handleMessage(m)
+          let customApi
+          if (typeof makeCustomApi === 'function') {
+            customApi = makeCustomApi(channel, params)
+          }
+
+          // Handle pending incoming messages.
+          // APIs are created before so handlers are already
+          // registered on the channel.
+          messageQueue.forEach((m) => {
+            // TODO Expose private handleMessage method
+            ;(channel as any)._handleMessage(m)
+          })
+
+          resolve([api, customApi])
         })
+      })
+    }
 
-        // Hand over control to the developer.
-        initCb(api, customApi)
-      }
+    initializedSdks.then(([sdk, customSdk]) =>
+      // Hand over control to the developer.
+      initCb(sdk, customSdk)
     )
   }
 }
