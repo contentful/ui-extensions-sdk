eb76fa731b74d096eacd89b97e3fe080c6b270f1
Renato Massao Yonamine <massao.yonamine@contentful.com>
2022-04-13 12:07:39 +0200

    feat: update type definition for startAutoResizer

--- lib/types/window.types.ts
+++ lib/types/window.types.ts
@@ -1,8 +1,12 @@
 export interface WindowAPI {
   /** Sets the iframe height to the given value in pixels or using scrollHeight if value is not passed */
   updateHeight: (height?: number) => void
-  /** Listens for DOM changes and updates height when the size changes. */
-  startAutoResizer: () => void
+  /**
+   * Listens for DOM changes and updates height when the size changes.
+   * @param {Object} opts - Options to be passed to auto resize
+   * @param {Boolean} opts.absoluteElements - Defines if auto resize should consider absolut elements
+   */
+  startAutoResizer: ({ absoluteElements }?: { absoluteElements?: boolean }) => void
   /** Stops resizing the iframe automatically. */
   stopAutoResizer: () => void
 }
