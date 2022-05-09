9ef7125c995f8c7f00fc2279052a41c3763665b1
Renato Massao Yonamine <1071799+massao@users.noreply.github.com>
2022-05-09 11:15:36 +0200

    Update lib/types/window.types.ts
    Co-authored-by: Andi Pätzold <andreas.paetzold@contentful.com>

--- lib/types/window.types.ts
+++ lib/types/window.types.ts
@@ -4,7 +4,7 @@ export interface WindowAPI {
   /**
    * Listens for DOM changes and updates height when the size changes.
    *
-   * When passing absoluteElements true, an infinite loop can happen if elements are always rendered bellow the height of the window.
+   * When passing absoluteElements true, an infinite loop can happen if elements are always rendered below the height of the window.
    * e.g. transformed elements or absolute elements with negative bottom
    * @param {Object} opts - Options to be passed to auto resize
    * @param {Boolean} opts.absoluteElements - Defines if auto resize should consider absolut elements
