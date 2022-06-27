a95161340e6b0f733fd7e84a7a2ff667307045ab
Lorenz Weiß <lorenz.weis@gmail.com>
2022-06-27 14:27:57 +0200

    fix: add options

--- lib/entry.ts
+++ lib/entry.ts
@@ -43,8 +43,8 @@ export default function createEntry(
     getSys() {
       return sys
     },
-    publish() {
-      return channel.call<void>('callEntryMethod', 'publish')
+    publish(options?: { skipValidation?: boolean }) {
+      return channel.call<void>('callEntryMethod', 'publish', options)
     },
     unpublish() {
       return channel.call<void>('callEntryMethod', 'unpublish')
