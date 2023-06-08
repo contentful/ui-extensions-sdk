155db7960c342f09bf74e767aeac89c8379f92cc
George Chios <george.chios@contentful.com>
2023-06-08 08:06:45 -0600

    change plainclient to cma client

--- lib/cma.ts
+++ lib/cma.ts
@@ -1,10 +1,10 @@
 import { createClient } from 'contentful-management'
-import type { PlainClientAPI } from 'contentful-management'
 import { createAdapter } from './cmaAdapter'
+import { CMAClient } from './types/cmaClient.types'
 import type { IdsAPI } from './types/api.types'
 import type { Channel } from './channel'
 
-export function createCMAClient(ids: IdsAPI, channel: Channel): PlainClientAPI {
+export function createCMAClient(ids: IdsAPI, channel: Channel): CMAClient {
   return createClient(
     { apiAdapter: createAdapter(channel) },
     {
