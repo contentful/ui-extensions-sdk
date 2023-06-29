3af8503197260a45a1d8804b7cc35d2a0a15ddd1
Yuri Mazursky <colomolo@users.noreply.github.com>
2023-06-29 14:37:03 +0200

    refactor: tweak rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,38 +5,36 @@ const commonjs = require('@rollup/plugin-commonjs')
 
 const pkg = require('./package.json')
 
-const makeConfigForOutput = (output, plugins = []) => ({
-  input: './lib/index.ts',
-  output,
-  plugins: [
-    typescript(),
-    terser({
-      format: {
-        comments: false,
-        ecma: 5,
-      },
-    }),
-    ...plugins,
-  ],
-})
-
-module.exports = [
-  makeConfigForOutput({
-    file: pkg.main,
-    format: 'umd',
-    name: 'contentfulExtension',
-    footer: 'globalThis.contentfulApp = globalThis.contentfulExtension;',
-    globals: {
-      'contentful-management': 'contentfulManagement',
+const defaultPlugins = [
+  typescript(),
+  terser({
+    format: {
+      comments: false,
+      ecma: 5,
     },
   }),
-  makeConfigForOutput(
-    {
+]
+
+module.exports = [
+  {
+    input: './lib/index.ts',
+    output: {
+      file: pkg.main,
+      format: 'umd',
+      name: 'contentfulExtension',
+      footer: 'globalThis.contentfulApp = globalThis.contentfulExtension;',
+    },
+    external: ['contentful-management'],
+    plugins: defaultPlugins,
+  },
+  {
+    input: './lib/index.ts',
+    output: {
       file: pkg.unpkg,
       format: 'umd',
       name: 'contentfulExtension',
       footer: 'globalThis.contentfulApp = globalThis.contentfulExtension;',
     },
-    [nodeResolve({ browser: true }), commonjs()]
-  ),
+    plugins: [...defaultPlugins, nodeResolve({ browser: true }), commonjs()],
+  },
 ]
