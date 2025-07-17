87cffdda49c58cb0210644d6b01b7cbea6d95252
Josh Lewis <josh.lewis@contentful.com>
2025-07-17 13:34:48 -0600

    feat(api): update makeEntryAPI to include release parameter in entry creation

--- lib/api.ts
+++ lib/api.ts
@@ -115,13 +115,13 @@ function makeEditorAPI(channel: Channel, data: any) {
 
 function makeEntryAPI(
   channel: Channel,
-  { locales, contentType, entry, fieldInfo }: ConnectMessage,
+  { locales, contentType, entry, fieldInfo, release }: ConnectMessage,
 ) {
   const createEntryField = (info: EntryFieldInfo) => new Field(channel, info, locales.default)
 
   return {
     contentType,
-    entry: createEntry(channel, entry, fieldInfo, createEntryField),
+    entry: createEntry(channel, entry, fieldInfo, createEntryField, release),
   }
 }
 
