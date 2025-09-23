e89780e02bb34abc45e11c254b952a721f66384a
Tyler <tyler.washington@contentful.com>
2025-09-23 15:18:02 -0600

    feat: add ASSET_SIDEBAR location constants
    - Add LOCATION_ASSET_SIDEBAR to locations.ts
    - Update Locations interface in api.types.ts
    - Part of EXT-6786: Asset Sidebar App Framework Location

--- lib/types/api.types.ts
+++ lib/types/api.types.ts
@@ -386,6 +386,7 @@ export interface Locations {
   LOCATION_ENTRY_FIELD: 'entry-field'
   LOCATION_ENTRY_FIELD_SIDEBAR: 'entry-field-sidebar'
   LOCATION_ENTRY_SIDEBAR: 'entry-sidebar'
+  LOCATION_ASSET_SIDEBAR: 'asset-sidebar'
   LOCATION_DIALOG: 'dialog'
   LOCATION_ENTRY_EDITOR: 'entry-editor'
   LOCATION_PAGE: 'page'
