5f5dc5a05a3c30127a37da4019c1565ec4c6bbd0
Michael Pineiro <michael.pineiro@contentful.com>
2026-03-09 16:22:35 -0600

    feat: [EXT-7182] sdk.exo namespace and shared exo types (#2478)

--- lib/types/api.types.ts
+++ lib/types/api.types.ts
@@ -23,6 +23,7 @@ import { EntryFieldInfo, FieldInfo } from './field.types'
 import { Adapter, KeyValueMap } from 'contentful-management'
 import { CMAClient } from './cmaClient.types'
 import { AgentAPI, AgentContext } from './agent.types'
+import { ExoSDK, UiMode } from './exo.types'
 
 /* User API */
 
@@ -353,6 +354,13 @@ export type AgentAppSDK<InstallationParameters extends KeyValueMap = KeyValueMap
   window: WindowAPI
 }
 
+export type ExperienceToolbarAppSDK<InstallationParameters extends KeyValueMap = KeyValueMap> =
+  Omit<BaseAppSDK<InstallationParameters, never, never>, 'ids'> & {
+    ids: Omit<IdsAPI, EntryScopedIds>
+    /** ExO (Experience Orchestration) SDK for experience-toolbar location */
+    exo: ExoSDK
+  }
+
 export type KnownAppSDK<
   InstallationParameters extends KeyValueMap = KeyValueMap,
   InstanceParameters extends KeyValueMap = KeyValueMap,
@@ -366,6 +374,7 @@ export type KnownAppSDK<
   | ConfigAppSDK<InstallationParameters>
   | HomeAppSDK<InstallationParameters>
   | AgentAppSDK<InstallationParameters>
+  | ExperienceToolbarAppSDK<InstallationParameters>
 
 /** @deprecated consider using {@link BaseAppSDK} */
 export type BaseExtensionSDK = BaseAppSDK
@@ -436,4 +445,8 @@ export interface ConnectMessage {
   release?: Release
   uiLanguageLocale: string
   agent?: AgentContext
+  /** ExO data; when present, SDK constructs sdk.exo */
+  exo?: {
+    uiMode?: UiMode
+  }
 }
